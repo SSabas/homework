0b73e28ec19a30f9460c91d5c7fa9c06365c5d5f
Sven Sabas <ssabas@gmail.com>
2019-06-27 11:02:39 +0100

    Finalised the code.

--- hw1/helpers.py
+++ hw1/helpers.py
@@ -11,93 +11,17 @@ Date:
 
 # ----------------------------- IMPORT LIBRARIES -------------------------------- #
 
-import json
 import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
+import os
+import gym
 from pytablewriter import MarkdownTableWriter
 
 EPS = 1e-8 # epsilon constant for numeric stability
 
 # ------------------ HELPER FUNCTIONS FOR IMITATION LEARNING -------------------- #
-
-
-def load_expert_data(filename, verbose=False):
-    """ Load the expert data from pickle saved in filename"""
-
-    expert_data = None
-    with open(filename, "rb") as f:
-        expert_data = pickle.load(f)
-
-    observations = expert_data["observations"].astype('float32')
-    actions = np.squeeze(expert_data["actions"].astype('float32'))
-    returns = expert_data["returns"].astype('float32')
-
-    if verbose:
-        # As a sanity check, print out the size of the training and test data.
-        print('observations shape: ', observations.shape)
-        print('actions shape: ', actions.shape)
-        print('mean return: ', np.mean(returns))
-
-    return observations, actions, returns
-
-
-def train_test_val_split(X, y,
-                         train_prop, val_prop,
-                         verbose=True):
-    """ Split the dataset (X, y) into train, validation, and test sets.
-    NB! Proportions should sum to 1.
-    Arguments:
-    X -- Feature matrix, shape of (N, D_in)
-    y -- Labels, shape of (N, num_classes)
-    train_prop -- Proportion of training data
-    val_prop -- Proportion of validation data
-    """
-    N_total = X.shape[0]
-    N_train = int(np.floor(N_total * train_prop))
-    N_val   = int(np.ceil(N_total * val_prop))
-    N_test  = N_total - N_train - N_val
-
-    assert(N_train + N_val + N_test == N_total)
-
-    # Split the data into test set and temporary set, which will be
-    # split into training and validation sets
-    X_tmp, X_test, y_tmp, y_test = train_test_split(X,
-                                                    y,
-                                                    test_size=N_test)
-
-    # Split X_tmp into training and validation
-    X_train, X_val, y_train, y_val = train_test_split(X_tmp,
-                                                      y_tmp,
-                                                      train_size=N_train)
-
-    if verbose:
-        print('Train data: ', X_train.shape, X_train.dtype)
-        assert(X_train.shape[0] == N_train)
-        print('Train labels: ', y_train.shape, y_train.dtype)
-        assert(y_train.shape[0] == N_train)
-
-        print('Validation data: ', X_val.shape, X_val.dtype)
-        assert(X_val.shape[0] == N_val)
-        print('Validation labels: ', y_val.shape, y_val.dtype)
-        assert(y_val.shape[0] == N_val)
-
-        print('Test data: ', X_test.shape, X_test.dtype)
-        assert(X_test.shape[0] == N_test)
-        print('Test labels: ', y_test.shape, y_test.dtype)
-        assert(y_test.shape[0] == N_test)
-
-    data = {
-        "X_train": X_train, "y_train": y_train,
-        "X_val": X_val, "y_val": y_val,
-        "X_test": X_test, "y_test": y_test,
-        "X_dev": X_dev, "y_dev": y_dev
-    }
-
-    return data
 
 
 def plot_bc_results():
@@ -125,7 +49,6 @@ def plot_bc_results():
 
         df.loc[envs[i]] = [np.mean(bc_data['returns']), np.std(bc_data['returns']), np.mean(expert_data['returns']), np.std(expert_data['returns'])]
 
-    if markdown_format == 'yes':
         writer = MarkdownTableWriter()
         writer.table_name = "add_index_column"
         writer.from_dataframe(df,add_index_column=True)
@@ -133,12 +56,13 @@ def plot_bc_results():
     return writer.write_table()
 
 
-def plot_bc_hyper_results(env, to_save='yes'):
+def plot_bc_hyper_results(env, bc_data=None, to_save='yes'):
 
     # Get data
-    with open(os.getcwd() + "/results/bc_hyperparameter/" + env + ".pkl", 'rb') as f:
-        bc_data = pickle.loads(f.read())
-        print('BC data loaded.')
+    if bc_data is None:
+        with open(os.getcwd() + "/results/bc_hyperparameter/" + env + ".pkl", 'rb') as f:
+            bc_data = pickle.loads(f.read())
+            print('BC data loaded.')
 
     with open(os.getcwd() + "/expert_data/" + env + ".pkl", 'rb') as f:
         expert_data = pickle.loads(f.read())
@@ -173,10 +97,127 @@ def plot_bc_hyper_results(env, to_save='yes'):
     plt.title("Learning Curve of %s" %env)
     plt.xlabel("Training Set Size"), plt.ylabel("Reward"), plt.legend(loc="best")
     plt.tight_layout()
-    plt.show()
 
-    if to_save='yes':
-        plt.savefig(os.getcwd() + '/results/bc_hyperparameter/' + env + '.pdf',
+    if to_save == 'yes':
+        plt.savefig(os.getcwd() + '/results/bc_hyperparameter/' + env + '.png',
                     bbox_inches='tight')
+
+    else:
+        plt.show()
+
+    return
+
+
+def evaluate_model(env, model, max_timesteps, num_rollouts):
+
+    # Identify the environment
+    agent = gym.make(env)
+
+    # Define the maximum number of timesteps to be evaluated in each rollout
+    if max_timesteps is None:
+        max_timesteps = agent.spec.timestep_limit
+
+    # Do the roll-outs
+
+    returns = []
+    observations = []
+    actions = []
+
+    for j in range(num_rollouts):
+        observation = agent.reset()
+        done = False
+        rollout_reward = 0.0
+        steps = 0
+
+        while not done and steps < max_timesteps:
+            observation = np.array(observation)
+
+            action = model.model.predict(x=observation[None, :], verbose=False)
+            observations.append(observation)
+            actions.append(action)
+            observation, reward, done, _ = agent.step(action)
+            rollout_reward += reward
+            steps += 1
+
+            if steps % 100 == 0:
+                print("%i/%i" % (steps, max_timesteps))
+
+        returns.append(rollout_reward)
+
+    print('Returns', returns)
+    print('Mean return', np.mean(returns))
+    print('Std of return', np.std(returns))
+
+    output = {'observations': np.array(observations),
+              'actions': np.array(actions),
+              'returns': np.array(returns)}
+
+    return output
+
+
+def plot_bc_dagger_comp_results(env, bc_data=None, dagger_data=None, to_save='yes'):
+
+    # Get data
+    if bc_data is None:
+        with open(os.getcwd() + "/results/bc_hyperparameter/" + env + ".pkl", 'rb') as f:
+            bc_data = pickle.loads(f.read())
+            print('BC data loaded.')
+
+    if dagger_data is None:
+        with open(os.getcwd() + "/results/dagger_evaluation/" + env + ".pkl", 'rb') as f:
+            dagger_data = pickle.loads(f.read())
+            print('Dagger data loaded.')
+
+    with open(os.getcwd() + "/expert_data/" + env + ".pkl", 'rb') as f:
+        expert_data = pickle.loads(f.read())
+        print('Expert data loaded.')
+
+    bc_mean = np.array([])
+    bc_std = np.array([])
+
+    dagger_mean = np.array([])
+    dagger_std = np.array([])
+
+    expert_mean = np.mean(expert_data['returns'])
+    expert_std = np.std(expert_data['returns'])
+
+    # Retrieve results
+    for i in sorted(bc_data.keys()):
+        print(i)
+        bc_mean = np.append(bc_mean, np.mean(bc_data[i]['returns']))
+        bc_std = np.append(bc_std, np.std(bc_data[i]['returns']))
+
+    for i in sorted(dagger_data.keys()):
+        print(i)
+        dagger_mean = np.append(dagger_mean, np.mean(dagger_data[i]['returns']))
+        dagger_std = np.append(dagger_std, np.std(dagger_data[i]['returns']))
+
+    # Draw lines
+    plt.plot(sorted(bc_data.keys()), bc_mean,
+             '--', color="midnightblue", label="BC Score")
+    plt.plot(sorted(dagger_data.keys()), dagger_mean,
+             color="darkolivegreen", label="Dagger Score")
+    plt.plot(sorted(bc_data.keys()), [expert_mean]*len(bc_mean),
+             color="darkred", label="Expert Score")
+
+    # Draw bands
+    plt.fill_between(sorted(bc_data.keys()), bc_mean - bc_std, bc_mean + bc_std,
+                     color="dodgerblue", alpha=0.3)
+    plt.fill_between(sorted(dagger_data.keys()), dagger_mean - dagger_std, dagger_mean + dagger_std,
+                     color="darkseagreen", alpha=0.3)
+    plt.fill_between(sorted(bc_data.keys()), [expert_mean - expert_std]*len(bc_mean), [expert_mean + expert_std]*len(bc_mean),
+                     color="lightcoral", alpha=0.3)
+
+    # Create plot
+    plt.title("Learning Curve of %s" %env)
+    plt.xlabel("Training Set Size"), plt.ylabel("Reward"), plt.legend(loc="best")
+    plt.tight_layout()
+
+    if to_save == 'yes':
+        plt.savefig(os.getcwd() + '/results/dagger_evaluation/' + env + '.png',
+                    bbox_inches='tight')
+
+    else:
+        plt.show()
 
     return
